2e3e68d12ba0c465f2e650c07973ed67501a8b40
Laura Jetten <l.jetten@portofrotterdam.com>
2021-06-10 17:01:29 +0200

    fixed minor bug

--- line_string_test.go
+++ line_string_test.go
@@ -192,6 +192,46 @@ func TestLineString_IntersectTriplePointReverse(t *testing.T) {
 
 }
 
+func TestLineString_IntersectTriplePointReverseSwapped(t *testing.T) {
+	l1p3 := Point{
+		Latitude:  51.93052146629074,
+		Longitude: 4.458684325218201,
+	}
+	l1p2 := Point{
+		Latitude:  51.930346153123985,
+		Longitude: 4.458823800086975,
+	}
+	l1p1 := Point{
+		51.930346153123985,
+		4.45824900196975,
+	}
+
+	l1 := LineString{[]Point{
+		l1p1, l1p2, l1p3,
+	}}
+
+	l2p1 := Point{
+		Latitude:  51.93040734742036,
+		Longitude: 4.458622634410858,
+	}
+
+	l2p2 := Point{
+		Latitude:  51.93043380979297,
+		Longitude: 4.459003508090973,
+	}
+
+	l2 := LineString{[]Point{
+		l2p1, l2p2,
+	}}
+
+	crossed, point := l2.Intersect(l1)
+	assert.True(t, crossed)
+	assert.Equal(t, Point{51.93041738656155, 4.458767128471792}, point)
+
+	t.Logf("crossed: %v and point is [ %v , %v ] \n", crossed, point.Longitude, point.Latitude)
+
+}
+
 func TestLineString_multiLineStringExtract(t *testing.T) {
 	l1p1 := Point{
 		Latitude:  51.93052146629074,
