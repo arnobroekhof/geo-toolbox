64932d33a855e0d8146e39fba8d01bf3de96621e
ljetten <ljetten@outlook.com>
2021-12-27 12:02:52 +0100

    Add tests and allow times to be reversed (#5)

--- speed_test.go
+++ speed_test.go
@@ -1,10 +1,11 @@
 package geo_toolbox
 
 import (
-	"github.com/stretchr/testify/assert"
 	"math"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestSpeed(t *testing.T) {
@@ -42,12 +43,47 @@ func TestSpeedMultiple_2Records(t *testing.T) {
 
 }
 
+func TestSpeedMultiple_2RecordsReversed(t *testing.T) {
+	rotterdam := Point{51.92250, 4.47917}
+	tn := time.Now()
+	t1 := tn.Add(-1 * time.Hour)
+	t2 := tn.Add(-2 * time.Hour)
+	amsterdam := Point{52.37403, 4.88969}
+
+	tpRotterdam := NewTimebasedPoint(t1, rotterdam)
+	tpAmsterdam := NewTimebasedPoint(t2, amsterdam)
+
+	kph := SpeedMultiple(tpRotterdam, tpAmsterdam)
+	assert.Equal(t, 1, len(kph))
+	assert.Equal(t, 58, int(math.Round(kph[0])))
+
+}
+
 func TestSpeedMultiple_3Records(t *testing.T) {
 	rotterdam := Point{51.92250, 4.47917}
 	tn := time.Now()
 	t1 := tn.Add(-3 * time.Hour)
 	t2 := tn.Add(-2 * time.Hour)
 	t3 := tn.Add(-1 * time.Hour)
+	amsterdam := Point{52.37403, 4.88969}
+	utrecht := Point{52.0908, 5.1222}
+
+	tpRotterdam := NewTimebasedPoint(t1, rotterdam)
+	tpAmsterdam := NewTimebasedPoint(t2, amsterdam)
+	tpUtrecht := NewTimebasedPoint(t3, utrecht)
+
+	kph := SpeedMultiple(tpRotterdam, tpAmsterdam, tpUtrecht)
+	assert.Equal(t, 2, len(kph))
+	assert.Equal(t, 58, int(math.Round(kph[0])))
+	assert.Equal(t, 35, int(math.Round(kph[1])))
+}
+
+func TestSpeedMultiple_3RecordsReversed(t *testing.T) {
+	rotterdam := Point{51.92250, 4.47917}
+	tn := time.Now()
+	t1 := tn.Add(-1 * time.Hour)
+	t2 := tn.Add(-2 * time.Hour)
+	t3 := tn.Add(-3 * time.Hour)
 	amsterdam := Point{52.37403, 4.88969}
 	utrecht := Point{52.0908, 5.1222}
 
